0faf272a65939306c24a8f748f3afc61b577930c
Vadym <vadymmykulych@gmail.com>
2025-01-09 12:55:02 +0200

    add data-table component

--- admin/src/components/index.ts
+++ admin/src/components/index.ts
@@ -1,3 +1,4 @@
 export * from "./ui";
 export * from "./layout";
 export * from "./header";
+export * from "./data-table";
--- admin/src/pages/user-management.tsx
+++ admin/src/pages/user-management.tsx
@@ -1,7 +1,43 @@
+import { DataTable } from "@/components/data-table";
+import { ColumnDef } from "@tanstack/react-table";
+
+export type User = {
+  id: number;
+  status: string;
+  name: string;
+  email: string;
+};
+
+export const columns: ColumnDef<User>[] = [
+  {
+    accessorKey: "id",
+    header: "ID",
+  },
+  {
+    accessorKey: "status",
+    header: "Status",
+  },
+  {
+    accessorKey: "name",
+    header: "Name",
+  },
+  {
+    accessorKey: "email",
+    header: "Email",
+  },
+];
+
+const users: User[] = [
+  { id: 1, status: "Success", name: "John Doe", email: "john@gmail.com" },
+  { id: 2, status: "Success", name: "Jane Smith", email: "jane@gmail.com" },
+  { id: 3, status: "Success", name: "Alice Johnson", email: "alice@gmail.com" },
+  { id: 4, status: "Success", name: "Bob Brown", email: "bob@gmail.com" },
+];
+
 export const UserManagementPage = () => {
   return (
     <div>
-      <p>user management</p>
+      <DataTable columns={columns} data={users} />
     </div>
   );
 };
