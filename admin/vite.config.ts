ddaa6dae4c35e945d0ffb8969722b13db3ed93ff
Vadym <vadymmykulych@gmail.com>
2025-01-15 17:04:28 +0200

    install vite inspect plugin

--- admin/vite.config.ts
+++ admin/vite.config.ts
@@ -1,10 +1,11 @@
 import path from "path";
 import react from "@vitejs/plugin-react-swc";
+import Inspect from "vite-plugin-inspect";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), Inspect({ build: true, outputDir: ".vite-inspect"})],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
