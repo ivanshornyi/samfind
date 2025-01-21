ec5ed409d8ab09e07147972aeeab935fb5f7c723
Liasotaandrii0 <liasotaandrii0@gmail.com>
2025-01-21 15:29:31 +0200

    info

--- client/src/components/pages/about/info.tsx
+++ client/src/components/pages/about/info.tsx
@@ -1,7 +1,24 @@
+"use client";
+
 import { Button } from "@/components/ui";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export const Info = () => {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    function handleScroll() {
+      if (window.scrollY > 200) {
+        setScrolled(true);
+      }
+    }
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
     <div className="relative font-manrope h-[824px] pt-[150px]">
       <Image
@@ -18,8 +35,14 @@ export const Info = () => {
       <Button variant="link" icon>
         Contact us
       </Button>
-      <h1 className="text-[96px] mt-[56px] leading-[96px] font-extrabold">
-        Transform Your Digital
+      <h1 className="text-[48px] mt-[56px] leading-[65px] font-extrabold max-w-[1180px]">
+        <span>
+          {`In Osio we're dedicated to creating solutions that respect privacy`}
+        </span>
+        {"  "}
+        <span
+          className={scrolled ? "" : "text-disabled"}
+        >{`while pushing the boundaries of what's possible.`}</span>
       </h1>
     </div>
   );
