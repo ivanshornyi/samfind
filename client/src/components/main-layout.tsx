559b70702cf6d1c26b39153629b9395b9e7438b0
Vadym <vadymmykulych@gmail.com>
2025-01-22 18:02:31 +0200

    hide footer for auth pages

--- client/src/components/main-layout.tsx
+++ client/src/components/main-layout.tsx
@@ -7,14 +7,14 @@ import { Footer, Header } from "@/components";
 export const MainLayout = ({ children }: { children: React.ReactNode }) => {
   const pathname = usePathname();
 
-  const hideHeader = pathname.startsWith("/auth");
+  const hideHeaderAndFooter = pathname.startsWith("/auth");
 
   return (
     <div className="max-w-[1440px] mx-auto">
-      {!hideHeader && <Header />}
+      {!hideHeaderAndFooter && <Header />}
 
-      <main className={`${hideHeader ? "" : "pt-20"} px-4 sm:px-5`}>{children}</main>
-      <Footer />
+      <main className={`${hideHeaderAndFooter ? "" : "pt-20"} px-4 sm:px-5`}>{children}</main>
+      {!hideHeaderAndFooter && <Footer />}
     </div>
   );
 };
