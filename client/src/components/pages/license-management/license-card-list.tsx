afb6b7ec60fe4a75ff452650ee1895cc0058e4f0
Vadym <vadymmykulych@gmail.com>
2025-01-22 17:40:32 +0200

    add ellipse to license management page

--- client/src/components/pages/license-management/license-card-list.tsx
+++ client/src/components/pages/license-management/license-card-list.tsx
@@ -29,7 +29,6 @@ const cards: LicenseManagement[] = [
   },
   {
     id: 2,
-
     title: "License Settings",
     description: "Tailor your license preferences to fit your needs.",
     actions: [
@@ -54,17 +53,17 @@ const cards: LicenseManagement[] = [
 
 export const LicenseCardList = () => {
   const starsImage = (
-      <Image
-        src="/license-management/stars.png"
-        alt="icon"
-        width={130}
-        height={128}
-      />
+    <Image
+      src="/license-management/stars.png"
+      alt="icon"
+      width={130}
+      height={128}
+    />
   );
 
   return (
     <div className="pt-[152px] md:pt-[200px] pb-[60px] md:pb-[120px]">
-      <div className="w-fit rounded-3xl bg-[#242424] py-[6.4px] px-[25.6px] mb-6 mx-auto md:mx-[0]">
+      <div className="w-fit rounded-3xl bg-[#242424] py-[6.4px] px-[25.6px] mb-6 mx-auto md:mx-[0] relative z-[11]">
         <span className="font-medium text-base text-[#CE9DF3]">
           Explore more
         </span>
@@ -76,13 +75,26 @@ export const LicenseCardList = () => {
         {cards.map((card, index) => (
           <React.Fragment key={index}>
             {index === 2 ? (
-              <div className="hidden md:flex items-center md:justify-start">{starsImage}</div>
+              <div className="hidden md:flex items-center md:justify-start">
+                {starsImage}
+              </div>
             ) : null}
             <LicenseCard card={card} />
-            {index === 2 ? <div className="md:hidden flex justify-end mt-14">{starsImage}</div> : null}
+            {index === 2 ? (
+              <div className="md:hidden flex justify-end mt-14">
+                {starsImage}
+              </div>
+            ) : null}
           </React.Fragment>
         ))}
       </div>
+      <Image
+        src="/license-management/ellipse-blue.png"
+        width={412}
+        height={200}
+        alt="Background illustration"
+        className="absolute top-[28px] left-0 z-[10]"
+      />
     </div>
   );
 };
