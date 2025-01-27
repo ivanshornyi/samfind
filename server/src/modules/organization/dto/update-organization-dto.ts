13354ab71b8e6388fb86cc5b6a9e5a650ed80e71
TarasVoievoda <taras@fatser.com>
2025-01-27 11:59:43 +0200

    feat: organization, licnse update endpoints

---
+++ server/src/modules/organization/dto/update-organization-dto.ts
@@ -0,0 +1,15 @@
+import { IsString, IsOptional, IsArray } from "class-validator";
+
+export class UpdateOrganizationDto {
+  @IsString()
+  @IsOptional()
+  name?: string;
+
+  @IsArray()
+  @IsOptional()
+  domains?: string[];
+
+  @IsArray()
+  @IsOptional()
+  availableEmails?: string[]; 
+}
--- server/src/modules/organization/organization.controller.ts
+++ server/src/modules/organization/organization.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Post, Get, Param, Body } from "@nestjs/common";
+import { Controller, Post, Get, Param, Body, Patch } from "@nestjs/common";
 
 import { ApiTags, ApiOperation } from "@nestjs/swagger";
 
 import { OrganizationService } from "./organization.service";
 
 import { CreateOrganizationDto } from "./dto/create-organization-dto";
+import { UpdateOrganizationDto } from "./dto/update-organization-dto";
 
 @ApiTags("Organization")
 @Controller("organization")
@@ -21,5 +22,11 @@ export class OrganizationController {
   @Get("/:id")
   async findOrganization(@Param("id") id: string) {
     console.log(id);
+  }
+
+  @ApiOperation({ summary: "Update organization" })
+  @Patch("/:id")
+  async updateOrganization(@Param("id") id: string, @Body() updateOrganizationDto: UpdateOrganizationDto) {
+    return await this.organizationService.update(id, updateOrganizationDto);
   }
 }
