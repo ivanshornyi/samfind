994bfc7cad64e96e4b48dc45f69b71b3a576e985
Liasotaandrii0 <liasotaandrii0@gmail.com>
2025-02-03 16:51:35 +0200

    fix: return invoice url when create subscription

--- server/src/modules/subscription/subscription.service.ts
+++ server/src/modules/subscription/subscription.service.ts
@@ -125,13 +125,13 @@ export class SubscriptionService {
       metadata,
     });
 
-    const paymentIntent = await this.stripeService.getPaymentIntention(
-      invoice.payment_intent as string,
-    );
+    // const paymentIntent = await this.stripeService.getPaymentIntention(
+    //   invoice.payment_intent as string,
+    // );
 
-    return { clientSecret: paymentIntent.client_secret };
+    // return { clientSecret: paymentIntent.client_secret };
 
-    // return { url: invoice.hosted_invoice_url };
+    return { url: invoice.hosted_invoice_url };
   }
 
   async addDiscount(
