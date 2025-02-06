74300e06e665992042303f5f4ee221011e88e00c
Liasotaandrii0 <liasotaandrii0@gmail.com>
2025-02-06 16:17:30 +0200

    feat: add try-catch

--- server/src/modules/cron/cron.service.ts
+++ server/src/modules/cron/cron.service.ts
@@ -19,171 +19,179 @@ export class CronService {
   @Cron("0 8 1 * *")
   //   @Cron("55 14 * * *")
   async handleFirstDayOfMonth() {
-    this.logger.log(
-      "Running on the 1st day of the month at 08:00 AM Create Invoices - start",
-    );
-    const today = new Date();
-    // const today = new Date("2025-03-01");
+    try {
+      this.logger.log(
+        "Running on the 1st day of the month at 08:00 AM Create Invoices - start",
+      );
+      const today = new Date();
+      // const today = new Date("2025-03-01");
 
-    const subscriptions = await this.prisma.subscription.findMany({
-      where: {
-        nextDate: {
-          gte: startOfDay(today),
-          lt: endOfDay(today),
+      const subscriptions = await this.prisma.subscription.findMany({
+        where: {
+          nextDate: {
+            gte: startOfDay(today),
+            lt: endOfDay(today),
+          },
+          isActive: true,
         },
-        isActive: true,
-      },
-      include: {
-        plan: true,
-        user: true,
-        license: {
-          include: {
-            _count: {
-              select: { activeLicenses: true },
+        include: {
+          plan: true,
+          user: true,
+          license: {
+            include: {
+              _count: {
+                select: { activeLicenses: true },
+              },
             },
           },
         },
-      },
-    });
-
-    const subscriptionIds = subscriptions.map((sub) => sub.id);
-
-    if (subscriptionIds.length > 0) {
-      await this.prisma.subscription.updateMany({
-        where: {
-          id: { in: subscriptionIds },
-        },
-        data: {
-          isInTrial: true,
-        },
-      });
-    }
-
-    for (let i = 0; i < subscriptions.length; i++) {
-      const subscription = subscriptions[i];
-      if (subscription.license._count.activeLicenses === 0) continue;
-
-      const discount = await this.prisma.discount.findFirst({
-        where: {
-          userId: subscription.userId,
-          stripeCouponId: null,
-          used: false,
-        },
       });
 
-      let discountId = undefined;
-      let discountAmount = undefined;
-      if (discount) {
-        discountAmount = discount.endAmount;
+      const subscriptionIds = subscriptions.map((sub) => sub.id);
 
-        if (
-          discount.endAmount >
-          subscription.license._count.activeLicenses * subscription.plan.price
-        ) {
-          discountAmount =
-            subscription.license._count.activeLicenses *
-            subscription.plan.price;
-          await this.prisma.discount.create({
-            data: {
-              userId: subscription.userId,
-              endAmount:
-                discount.endAmount -
-                subscription.license._count.activeLicenses *
-                  subscription.plan.price,
-            },
-          });
-        }
-
-        const stripeDiscount =
-          await this.stripeService.createCoupon(discountAmount);
-
-        discountId = stripeDiscount.id;
-
-        await this.prisma.discount.update({
-          where: { id: discount.id },
+      if (subscriptionIds.length > 0) {
+        await this.prisma.subscription.updateMany({
+          where: {
+            id: { in: subscriptionIds },
+          },
           data: {
-            stripeCouponId: discountId,
+            isInTrial: true,
           },
         });
       }
 
-      const metadata = {
-        quantity: subscription.license._count.activeLicenses,
-        subscriptionId: subscription.id,
-        stripeCouponId: discountId,
-        discountAmount,
-      };
+      for (let i = 0; i < subscriptions.length; i++) {
+        const subscription = subscriptions[i];
+        if (subscription.license._count.activeLicenses === 0) continue;
 
-      await this.stripeService.createAndPayInvoice({
-        customerId: subscription.user.stripeCustomerId,
-        priceId: subscription.plan.stripePriceId,
-        quantity: subscription.license._count.activeLicenses,
-        couponId: discountId,
-        description: `Plan - ${subscription.plan.type} - ${subscription.plan.period}. Quantity - ${subscription.license._count.activeLicenses}. ${discount ? `Discount: ${discount.endAmount / 100}$.` : ""}`,
-        metadata,
-        pay: true,
-      });
+        const discount = await this.prisma.discount.findFirst({
+          where: {
+            userId: subscription.userId,
+            stripeCouponId: null,
+            used: false,
+          },
+        });
+
+        let discountId = undefined;
+        let discountAmount = undefined;
+        if (discount) {
+          discountAmount = discount.endAmount;
+
+          if (
+            discount.endAmount >
+            subscription.license._count.activeLicenses * subscription.plan.price
+          ) {
+            discountAmount =
+              subscription.license._count.activeLicenses *
+              subscription.plan.price;
+            await this.prisma.discount.create({
+              data: {
+                userId: subscription.userId,
+                endAmount:
+                  discount.endAmount -
+                  subscription.license._count.activeLicenses *
+                    subscription.plan.price,
+              },
+            });
+          }
+
+          const stripeDiscount =
+            await this.stripeService.createCoupon(discountAmount);
+
+          discountId = stripeDiscount.id;
+
+          await this.prisma.discount.update({
+            where: { id: discount.id },
+            data: {
+              stripeCouponId: discountId,
+            },
+          });
+        }
+
+        const metadata = {
+          quantity: subscription.license._count.activeLicenses,
+          subscriptionId: subscription.id,
+          stripeCouponId: discountId,
+          discountAmount,
+        };
+
+        await this.stripeService.createAndPayInvoice({
+          customerId: subscription.user.stripeCustomerId,
+          priceId: subscription.plan.stripePriceId,
+          quantity: subscription.license._count.activeLicenses,
+          couponId: discountId,
+          description: `Plan - ${subscription.plan.type} - ${subscription.plan.period}. Quantity - ${subscription.license._count.activeLicenses}. ${discount ? `Discount: ${discount.endAmount / 100}$.` : ""}`,
+          metadata,
+          pay: true,
+        });
+      }
+
+      this.logger.log(
+        "Running on the 1st day of the month at 08:00 AM Create Invoices - end",
+      );
+    } catch (error) {
+      this.logger.error("Error Paying Subscriptions", error);
     }
-
-    this.logger.log(
-      "Running on the 1st day of the month at 08:00 AM Create Invoices - end",
-    );
   }
 
   @Cron("0 8 6 * *")
   //   @Cron("40 15 * * *")
   async handleSixthDayOfMonth() {
-    this.logger.log(
-      "Running on the 6th day of the month at 08:00 AM Check Unpaid Subscriptions - start",
-    );
+    try {
+      this.logger.log(
+        "Running on the 6th day of the month at 08:00 AM Check Unpaid Subscriptions - start",
+      );
 
-    const subscriptions = await this.prisma.subscription.findMany({
-      where: {
-        isInTrial: true,
-      },
-      include: { user: true },
-    });
-
-    const subscriptionIds = subscriptions.map((sub) => sub.id);
-    const licenseIds = subscriptions.map((sub) => sub.licenseId);
-
-    if (subscriptionIds.length > 0) {
-      await this.prisma.subscription.updateMany({
+      const subscriptions = await this.prisma.subscription.findMany({
         where: {
-          id: { in: subscriptionIds },
+          isInTrial: true,
         },
-        data: {
-          isInTrial: false,
-          isActive: false,
-        },
+        include: { user: true },
       });
 
-      await this.prisma.license.updateMany({
-        where: {
-          id: { in: licenseIds },
-        },
-        data: {
-          status: LicenseStatus.inactive,
-        },
-      });
-    }
+      const subscriptionIds = subscriptions.map((sub) => sub.id);
+      const licenseIds = subscriptions.map((sub) => sub.licenseId);
 
-    for (let i = 0; i < subscriptions.length; i++) {
-      const subscription = subscriptions[i];
-      const response = await this.stripeService.getUserInvoices(
-        subscription.user.stripeCustomerId,
-        1,
+      if (subscriptionIds.length > 0) {
+        await this.prisma.subscription.updateMany({
+          where: {
+            id: { in: subscriptionIds },
+          },
+          data: {
+            isInTrial: false,
+            isActive: false,
+          },
+        });
+
+        await this.prisma.license.updateMany({
+          where: {
+            id: { in: licenseIds },
+          },
+          data: {
+            status: LicenseStatus.inactive,
+          },
+        });
+      }
+
+      for (let i = 0; i < subscriptions.length; i++) {
+        const subscription = subscriptions[i];
+        const response = await this.stripeService.getUserInvoices(
+          subscription.user.stripeCustomerId,
+          1,
+        );
+        if (!response.data.length) continue;
+
+        await this.mailService.sendWarningLicenseDeactivated(
+          subscription.user.email,
+          response.data[0].hosted_invoice_url,
+        );
+      }
+
+      this.logger.log(
+        "Running on the 6th day of the month at 08:00 AM Check Unpaid Subscriptions - end",
       );
-      if (!response.data.length) continue;
-
-      await this.mailService.sendWarningLicenseDeactivated(
-        subscription.user.email,
-        response.data[0].hosted_invoice_url,
-      );
+    } catch (error) {
+      this.logger.error("Error deactivating Subscriptions", error);
     }
-
-    this.logger.log(
-      "Running on the 6th day of the month at 08:00 AM Check Unpaid Subscriptions - end",
-    );
   }
 }
