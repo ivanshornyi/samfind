3abb38af7931ac97ca5357120dc12e0882865af6
FunnyCoder345 <nazarii2314@gmail.com>
2025-02-06 14:25:17 +0200

    fix(client): revert next config

--- client/next.config.ts
+++ client/next.config.ts
@@ -4,14 +4,6 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: "http://www.onsio.io/api/:path*",
-      },
-    ];
-  },
 };
 
 export default nextConfig;
