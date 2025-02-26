a15a2e74b0b12057371358fdd13bc29442653b40
FunnyCoder345 <nazarii2314@gmail.com>
2025-02-26 14:39:23 +0200

    ADD: Order Placement, order match by auto, order query, order trade v1

--- server-trade/src/modules/orders-stock/orders-stock.service.ts
+++ server-trade/src/modules/orders-stock/orders-stock.service.ts
@@ -1,6 +1,20 @@
 import { BadRequestException, ConflictException, Injectable } from "@nestjs/common"
 import { PrismaService } from "nestjs-prisma"
+import { $Enums } from "@prisma/client";
 import { CreateStockOrderDto } from "./dto/create-order-dto"
+
+interface Order {
+  stockId: string;
+  userId: string;
+  type: $Enums.OrderType;
+  quantity: number;
+  offeredPrice: number;
+  paymentId: string | null;
+  id: string;
+  status: $Enums.OrderStatus;
+  createdAt: Date;
+  updatedAt: Date;
+}
 
 @Injectable()
 export class StockOrdersService {
@@ -14,23 +28,19 @@ export class StockOrdersService {
     if (!stockId || !userId) throw new BadRequestException("StockID and UserID is required in order creation.")
     try {
       const stockOrder = await this.prisma.$transaction(async (prisma) => {
-        const stock = await prisma.stock.findFirst({
-          where: { id: stockId }
-        })
+        const stock = await prisma.stock.findFirst({ where: { id: stockId } })
+        const user = await prisma.user.findFirst({ where: { id: userId } })
 
-        const user = await prisma.user.findFirst({
-          where: { id: userId }
-        })
-
-        const existingOrder = await this.prisma.orderStock.findFirst({
-          where: {
-            stockId,
-            userId,
-          },
-        })
-
-        if (existingOrder) throw new BadRequestException("You cannot duplicate an order for the same StockID and UserID.")
         if (!stock || !user) throw new BadRequestException("Stock or User wasn`t created or properly assigned.")
+
+        // const existingOrder = await this.prisma.orderStock.findFirst({
+        //   where: {
+        //     stockId,
+        //     userId,
+        //   },
+        // })
+
+        // if (existingOrder) throw new BadRequestException("You cannot duplicate an order for the same StockID and UserID.")
 
         const stockOrder = await prisma.orderStock.create({
           data: {
@@ -53,6 +63,13 @@ export class StockOrdersService {
             price: offeredPrice
           }
         })
+
+        const matchingOrder = await this.checkMatchingOrder(stockOrder)
+        if (matchingOrder) {
+          await this.performTrade(stockOrder, matchingOrder)
+          // we should return a created order with COMPLETED status
+          return { ...stockOrder, status: "COMPLETED" }
+        }
 
         return stockOrder
       })
@@ -127,5 +144,96 @@ export class StockOrdersService {
       },
       data: stockOrders
     }
+  }
+
+  /**
+    |============================
+    | UTILS FN
+    |============================
+  */
+
+  async checkMatchingOrder(newOrder: Order) {
+    const { stockId, type, quantity, offeredPrice } = newOrder
+
+    if (type === "SELL") {
+      return await this.prisma.orderStock.findFirst({
+        where: {
+          stockId,
+          type: "BUY",
+          status: "PENDING",
+          quantity,
+          offeredPrice: { gte: offeredPrice }
+        },
+        orderBy: [
+          { offeredPrice: "desc" }, // the highest price
+          { createdAt: "asc" } // the earliest placed order
+        ]
+      })
+    } else if (type === "BUY") {
+      return await this.prisma.orderStock.findFirst({
+        where: {
+          stockId,
+          type: "SELL",
+          status: "PENDING",
+          quantity,
+          offeredPrice: { lte: offeredPrice }
+        },
+        orderBy: [
+          { offeredPrice: "asc" }, // the minimal price
+          { createdAt: "asc" } // the earliest placed order
+        ]
+      })
+    }
+
+    return null
+  }
+
+  async performTrade(newOrder: Order, matchingOrder: Order) {
+    const { type: newOrderType, userId: newUserId, stockId, quantity, offeredPrice: newPrice } = newOrder
+    const { id: matchingOrderId, userId: matchingUserId, offeredPrice: matchingPrice } = matchingOrder
+
+    // determine price of person, who was in query earlier
+    const tradePrice = newOrder.createdAt < matchingOrder.createdAt ? newPrice : matchingPrice
+
+    await this.prisma.$transaction(async (prisma) => {
+      // update of stock-orders status
+      await prisma.orderStock.update({
+        where: { id: newOrder.id },
+        data: { status: "COMPLETED" }
+      })
+      await prisma.orderStock.update({
+        where: { id: matchingOrderId },
+        data: { status: "COMPLETED" }
+      })
+
+      // update transaction-history for both orders
+
+      // new order history
+      await prisma.transactionHistory.create({
+        data: {
+          stockId,
+          userId: newUserId,
+          orderId: newOrder.id,
+          type: newOrderType === "SELL" ? "SALE" : "PURCHASE",
+          quantity,
+          price: tradePrice
+        }
+      })
+
+      // match order history
+      await prisma.transactionHistory.create({
+        data: {
+          stockId,
+          userId: matchingUserId,
+          orderId: matchingOrderId,
+          type: newOrderType === "SELL" ? "PURCHASE" : "SALE",
+          quantity,
+          price: tradePrice
+        }
+      })
+
+      // here we need to update stocs quantity of users
+      // decrease from seller, increase in buyer
+    })
   }
 }
