0ad98ce1db3f9d34fad64a1b14cf8706e32baf08
Liasotaandrii0 <liasotaandrii0@gmail.com>
2025-03-03 14:41:20 +0200

    discount

--- server/src/modules/wallet/wallet.module.ts
+++ server/src/modules/wallet/wallet.module.ts
@@ -4,9 +4,14 @@ import { PrismaModule } from "../prisma/prisma.module";
 import { WalletController } from "./wallet.controller";
 import { WalletService } from "./wallet.service";
 import { SubscriptionModule } from "../subscription/subscription.module";
+import { StripeModule } from "../stripe/stripe.module";
 
 @Module({
-  imports: [PrismaModule, forwardRef(() => SubscriptionModule)],
+  imports: [
+    PrismaModule,
+    forwardRef(() => SubscriptionModule),
+    forwardRef(() => StripeModule),
+  ],
   controllers: [WalletController],
   providers: [WalletService],
   exports: [WalletService],
--- server/src/modules/wallet/wallet.service.ts
+++ server/src/modules/wallet/wallet.service.ts
@@ -1,5 +1,7 @@
 import {
   BadRequestException,
+  forwardRef,
+  Inject,
   Injectable,
   NotFoundException,
 } from "@nestjs/common";
@@ -8,16 +10,35 @@ import { PrismaService } from "../prisma/prisma.service";
 import { UpdateWalletDto } from "./dto/update-user-license-dto";
 import { BalanceType, TransactionType } from "@prisma/client";
 import { SubscriptionService } from "../subscription/subscription.service";
+import { StripeService } from "../stripe/stripe.service";
 
 @Injectable()
 export class WalletService {
   constructor(
     private readonly prisma: PrismaService,
     private readonly subscriptionService: SubscriptionService,
+    @Inject(forwardRef(() => StripeService))
+    private readonly stripeService: StripeService,
   ) {}
 
   async getUserWallet(userId: string) {
-    return await this.prisma.wallet.findUnique({ where: { userId } });
+    const wallet = await this.prisma.wallet.findUnique({
+      where: { userId },
+      include: { user: true },
+    });
+
+    if (!wallet) throw new NotFoundException("Wallet not found");
+
+    const stripeUser = await this.stripeService.getCustomer(
+      wallet.user.stripeCustomerId,
+    );
+    if (!stripeUser) throw new NotFoundException("Stripe User not found");
+
+    return {
+      ...wallet,
+      discountAmount: Math.abs(stripeUser.balance),
+      user: undefined,
+    };
   }
 
   async updateWallet({
@@ -29,27 +50,29 @@ export class WalletService {
     if (!discountAmount && !bonusAmount && !sharesAmount)
       throw new BadRequestException("No data to change");
 
-    const wallet = await this.prisma.wallet.findUnique({ where: { id } });
+    const wallet = await this.prisma.wallet.findUnique({
+      where: { id },
+      include: { user: true },
+    });
 
     if (!wallet) throw new NotFoundException("Wallet not found");
 
-    if (discountAmount && bonusAmount) {
+    if (discountAmount && (bonusAmount || bonusAmount === 0)) {
       if (discountAmount > wallet.discountAmount) {
-        await this.prisma.walletTransaction.create({
-          data: {
-            userId: wallet.userId,
-            walletId: wallet.id,
-            amount: discountAmount - wallet.discountAmount,
-            transactionType: TransactionType.income,
-            balanceType: BalanceType.discount,
-            description: "Transfer from a bonus balance",
-          },
-        });
-
         await this.subscriptionService.addDiscount({
           discountAmount,
           userId: wallet.userId,
         });
+        // await this.prisma.walletTransaction.create({
+        //   data: {
+        //     userId: wallet.userId,
+        //     walletId: wallet.id,
+        //     amount: discountAmount - wallet.discountAmount,
+        //     transactionType: TransactionType.income,
+        //     balanceType: BalanceType.discount,
+        //     description: "Transfer from a bonus balance",
+        //   },
+        // });
       }
 
       if (bonusAmount < wallet.bonusAmount) {
@@ -64,7 +87,7 @@ export class WalletService {
           },
         });
       }
-    } else if (sharesAmount && bonusAmount) {
+    } else if (sharesAmount && (bonusAmount || bonusAmount === 0)) {
       if (sharesAmount > wallet.sharesAmount) {
         await this.prisma.walletTransaction.create({
           data: {
@@ -104,10 +127,30 @@ export class WalletService {
         });
       }
     }
+    // else if (discountAmount || discountAmount === 0) {
+    //   await this.prisma.walletTransaction.create({
+    //     data: {
+    //       userId: wallet.userId,
+    //       walletId: wallet.id,
+    //       amount: wallet.discountAmount - discountAmount,
+    //       transactionType: TransactionType.expense,
+    //       balanceType: BalanceType.discount,
+    //       description: "Pay Invoice",
+    //     },
+    //   });
+    // }
+    const stripeUser = await this.stripeService.getCustomer(
+      wallet.user.stripeCustomerId,
+    );
+    if (!stripeUser) throw new NotFoundException("Stripe User not found");
 
     return await this.prisma.wallet.update({
       where: { id },
-      data: { discountAmount, bonusAmount, sharesAmount },
+      data: {
+        discountAmount: Math.abs(stripeUser.balance),
+        bonusAmount,
+        sharesAmount,
+      },
     });
   }
 }
