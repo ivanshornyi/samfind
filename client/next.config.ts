f548a02c7460723d9a5dccf1f18bc87b7cd512c7
FunnyCoder345 <nazarii2314@gmail.com>
2025-03-06 16:21:26 +0200

    INIT: Next config returned

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,54 +1,18 @@
-// import type { NextConfig } from "next";
+import type { NextConfig } from "next";
 
-// const nextConfig: NextConfig = {
-//   eslint: {
-//     ignoreDuringBuilds: true,
-//   },
-//   webpack: (config) => {
-//     config.module.rules.push({
-//       test: /\.svg$/,
-//       use: ['@svgr/webpack', 'url-loader'],
-//     });
-
-//     return config;
-//   },
-//   reactStrictMode: true,
-// };
-
-// export default nextConfig;
-
-const isProd = process.env.NODE_ENV === 'production'
-
-const remotePatterns = [
-  {
-    protocol: 'https',
-    hostname: `**.${process.env.ROOT_DOMAIN}`,
-    port: ''
-  }
-]
-
-if (!isProd) {
-  remotePatterns.push({
-    hostname: 'localhost',
-    port: '',
-    protocol: 'http'
-  })
-}
-
-module.exports = {
-  webpack(config) {
+const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/,
-      use: ['@svgr/webpack']
-    })
+      use: ['@svgr/webpack', 'url-loader'],
+    });
 
-    return config
+    return config;
   },
-  images: {
-    remotePatterns
-  },
-  eslint: {
-    ignoreDuringBuilds: true
-  },
-  assetPrefix: isProd ? process.env.PUBLIC_URL : undefined
-}
+  reactStrictMode: true,
+};
+
+export default nextConfig;
