d7491a2fc657d71f017af8fa158675e0cba01291
VadymMykulych1 <vadymmykulych4@gmail.com>
2025-03-10 14:32:01 +0200

    fix hydration issue on wallet page

--- client/src/app/(private)/account/wallet/_components/maximize-info.tsx
+++ client/src/app/(private)/account/wallet/_components/maximize-info.tsx
@@ -5,14 +5,14 @@ import Image from "next/image";
 export const MaximizeInfo = () => {
   return (
     <div className="flex p-0 flex-col justify-center items-center rounded-2xl relative w-full h-[171px] bg-[#242424] gap-5">
-      <p className="text-[#CE9DF3] text-[20px] leading-[27px] font-semibold flex z-10">
+      <div className="text-[#CE9DF3] text-[20px] leading-[27px] font-semibold flex z-10">
         <div className="flex pt-[2px]">
           <ChevronsRight />
           <ChevronsRight className="ml-[-10px]" />
         </div>
 
         <span>Maximize Your Wallet!</span>
-      </p>
+      </div>
       <h2 className="text-[20px] leading-[27px] font-bold z-10">
         Buy Shares & Become a Part of Our Success
       </h2>
